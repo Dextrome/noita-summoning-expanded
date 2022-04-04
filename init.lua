c1c0bf12d68a83a71ccc9de31231a9ecffd77a35
dextrome <dextrome@gmail.com>
2022-04-04 18:34:11 +0200

    waterskull

--- init.lua
+++ init.lua
@@ -14,10 +14,9 @@ function OnPlayerSpawned( player_entity )
 
 end
 
-
+--ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/dextrome_summon_spells/data/scripts/summon_egg_elemental.lua")
 ModLuaFileAppend( "data/scripts/item_spawnlists.lua", "mods/dextrome_summon_spells/data/scripts/inject_eggs.lua")
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/dextrome_summon_spells/data/scripts/summon_egg_frog.lua")
---ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/dextrome_summon_spells/data/scripts/summon_egg_elemental.lua")
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/dextrome_summon_spells/data/scripts/summon_egg_tappura.lua")
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/dextrome_summon_spells/data/scripts/summon_egg_support.lua")
 ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/dextrome_summon_spells/data/scripts/summon_egg_passive.lua")
